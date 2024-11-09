2d1feee65e1df779d757932c347a47172b12ba7e
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-09 12:22:32 +0600

    setting up twitter

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
+
 export default function Layout() {
   return (
     <div className="w-10/12 mx-auto">
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,35 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import auth from "../../firebase";
+import { useState } from "react";
+
 
 export default function Login() {
+
+    const [users, setUser] = useState(null)
+
+    console.log(users)
+    const handleLoginWothGoogle=()=>{
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth,provider)
+        .then((result) => {
+            console.log(result.user);
+            setUser(result.user)
+            
+        }).catch(error => {
+            console.log('ERROR',error);
+            setUser(null);
+        })
+    }
   return (
     <div>
-      Login
+      <button onClick={handleLoginWothGoogle} className="btn btn-primary">Login With Google</button>
+
+      {users && (
+                <div>
+                    <h1>Name: {users.displayName}</h1>
+                    <p>Email: {users.email}</p>
+                </div>
+            )}
     </div>
   )
 }
